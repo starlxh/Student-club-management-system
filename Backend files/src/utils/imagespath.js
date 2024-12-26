13d15b5345dc50f9d45389bfaeb1b65c72c53468
22wjLiu <wanyaba@outlook.com>
2024-12-26 21:03:49 +0800

    整合文件

--- Backend files/src/utils/imagespath.js
+++ Backend files/src/utils/imagespath.js
@@ -1,4 +1,4 @@
 export function getImgUrlHeader() {
-  return 'http://localhost:9999'
-  // return 'http://www.shantouliu.site:4485'
+  // return 'http://localhost:9999'
+  return 'http://www.shantouliu.site:4485'
 }
